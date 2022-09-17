5f60289b9cc2f8744c1d405081bac04630b0bf19
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-09-17 21:41:15 +0700

    добавлен JSDoc

--- helpers/args.js
+++ helpers/args.js
@@ -1,8 +1,13 @@
+/**
+ *
+ * @param {Array} args массив аргументов командной строки
+ * @returns {{key:value}} объект пар ключ-значение
+ */
 export function parseArgs(args) {
   const parsedArgs = {};
   const tokens = args.join(" ").split("-").slice(1);
 
-  tokens.forEach(token => {
+  tokens.forEach((token) => {
     const [arg, value] = token.trim().split(" ");
     parsedArgs[arg] = value || true;
   });
