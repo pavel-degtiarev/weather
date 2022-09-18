b58e03fd1b1e401cf32877779ed5f4f265717b41
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-09-18 22:44:23 +0700

    Реализованы сохранение и чтение значений из Settings

--- services/storage.js
+++ services/storage.js
@@ -1,5 +1,36 @@
 import { homedir } from "os";
+import { join } from "path";
+import { promises } from "fs";
 
-export function saveKeyValue(key, value) {
-  console.log(homedir(), key, value);
+const SETTINGS_FILE_NAME = "weather-data.json";
+const settingsPath = join(homedir(), SETTINGS_FILE_NAME);
+
+export async function saveKeyValue(key, value) {
+  const settings = await readSettingsFile(settingsPath);
+  settings[key] = value;
+  await promises.writeFile(settingsPath, JSON.stringify(settings));
+}
+
+export async function readKeyValue(key) {
+  const settings = await readSettingsFile(settingsPath);
+  return settings[key];
+return 
+}
+
+async function readSettingsFile(path) {
+  let settings = null;
+  if (await isExist(path)) {
+    const raw = await promises.readFile(path);
+    settings = JSON.parse(raw);
+  }
+  return settings;
+}
+
+async function isExist(path) {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
